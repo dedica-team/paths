57e4ed9f255c22392dee7b99091942ceee1442af
DESKTOP-QLS9EQQ\Nutzer <d>
2021-02-25 12:28:14 +0100

    growing flower

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import {motion} from "framer-motion";
-import {PathGenerator} from "./PathGenerator";
+import {coords, PathGenerator} from "./PathGenerator";
 
-const icon = {
+const twig = {
     hidden: {
         opacity: 0,
         pathLength: 0,
         fill: 'none',
-        strokeWidth: 10,
-        stroke: 'orange'
+        strokeWidth: 20,
+        stroke: 'darkorange'
     },
     visible: {
         opacity: 1,
@@ -17,26 +17,78 @@ const icon = {
     }
 };
 
+const blossom = {
+    hidden: {
+        opacity: 0,
+        fill: 'none',
+        strokeWidth: 10,
+        r: 5
+    },
+    visible: {
+        opacity: 1,
+        fill: 'white',
+        r: 30,
+    }
+};
+
+
+const blossomCore = {
+    hidden: {
+        opacity: 0,
+        fill: 'none',
+        strokeWidth: 10,
+        r: 5
+    },
+    visible: {
+        opacity: 1,
+        fill: 'darkorange',
+        r: 20,
+    }
+};
+
 
 const pathGenerator = new PathGenerator(100);
 
-
+const target: coords = pathGenerator.generatePath(100, 100, ['f', 'l', 'f', 'r']);
+const path = target.pathParts.join(' ')
+console.log(target)
 export const Example = () => (
     <motion.svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 300 300"
         className="item"
     >
-        <motion.path
-            d={pathGenerator.generatePath(100, 100, ['f', 'l', 'r', 'f'])}
-            variants={icon}
-            initial="hidden"
-            animate="visible"
-            fill={''}
-            transition={{
-                default: {duration: 2, ease: "easeInOut"},
-                fill: {duration: 2, ease: [1, 0, 0.8, 1]}
-            }}
-        />
+        <motion.g>
+            <motion.path
+                d={path}
+                variants={twig}
+                initial="hidden"
+                animate="visible"
+                fill={''}
+                transition={{
+                    default: {duration: 2, ease: "easeInOut"},
+                    fill: {duration: 2, ease: [1, 0, 0.8, 1]},
+                }}
+            />
+            <motion.g>
+                <motion.circle variants={blossom}
+                               initial="hidden"
+                               animate="visible"
+                               r={30} x={target.x} y={target.y}
+                               transition={{
+                                   default: {duration: 1, ease: "easeInOut"},
+                                   delay:1.8
+                               }}/>
+                <motion.circle variants={blossomCore}
+                               initial="hidden"
+                               animate="visible"
+                               r={10} x={target.x} y={target.y}
+                               transition={{
+                                   default: {duration: 1, ease: "easeInOut"},
+                                   delay:1.8
+                               }}/>
+            </motion.g>
+        </motion.g>
+
     </motion.svg>
 );
--- src/PathGenerator.ts
+++ src/PathGenerator.ts
@@ -2,7 +2,7 @@ export interface coords {
     x: number;
     y: number;
     direction: string;
-    pathPart: string[];
+    pathParts: string[];
 }
 
 export class PathGenerator {
@@ -14,110 +14,110 @@ export class PathGenerator {
     }
 
     nextCoordinates = (lastCoords: coords, newDirection: string): coords => {
-
         let coordinates: coords = {
             x: lastCoords.x,
             y: lastCoords.y,
             direction: '',
-            pathPart: lastCoords.pathPart
+            pathParts: lastCoords.pathParts
         };
 
         //forward
         if (newDirection === 'f') {
             if (lastCoords.direction === 'up') {
                 coordinates.y -= this.increment;
-                coordinates.pathPart.push('l 0 -' + 0.8 * this.increment + ' ');
-                coordinates.pathPart.push('l 0 -' + 0.2 * this.increment + ' ');
+                coordinates.pathParts.push('l 0 -' + 0.8 * this.increment + ' ');
+                coordinates.pathParts.push('l 0 -' + 0.2 * this.increment + ' ');
                 coordinates.direction = 'up';
             }
             if (lastCoords.direction === 'down') {
                 coordinates.y += this.increment;
-                coordinates.pathPart.push('l 0 ' + 0.8 * this.increment + ' ');
-                coordinates.pathPart.push('l 0 ' + 0.2 * this.increment + ' ');
+                coordinates.pathParts.push('l 0 ' + 0.8 * this.increment + ' ');
+                coordinates.pathParts.push('l 0 ' + 0.2 * this.increment + ' ');
                 coordinates.direction = 'down';
             }
             if (lastCoords.direction === 'left') {
+
                 coordinates.x -= this.increment;
-                coordinates.pathPart.push('l -' + 0.8 * this.increment + ' 0');
-                coordinates.pathPart.push('l -' + 0.2 * this.increment + ' 0');
+                coordinates.pathParts.push('l -' + 0.8 * this.increment + ' 0');
+                coordinates.pathParts.push('l -' + 0.2 * this.increment + ' 0');
                 coordinates.direction = 'left';
             }
             if (lastCoords.direction === 'right') {
                 coordinates.x += this.increment;
-                coordinates.pathPart.push('l ' + 0.8 * this.increment + ' 0');
-                coordinates.pathPart.push('l ' + 0.2 * this.increment + ' 0');
+                coordinates.pathParts.push('l ' + 0.8 * this.increment + ' 0');
+                coordinates.pathParts.push('l ' + 0.2 * this.increment + ' 0');
                 coordinates.direction = 'right';
             }
         }
 
         //turn left
         if (newDirection === 'l') {
-            let pop = coordinates.pathPart.pop(); //remove
+            let pop = coordinates.pathParts.pop(); //remove
             if (pop) {
-                coordinates.pathPart.push(pop.replace('l', 'q')); //turn former end into control point
+                coordinates.pathParts.push(pop.replace('l', 'q')); //turn former end into control point
             }
             if (lastCoords.direction === 'up') {
                 coordinates.x -= this.increment;
-                coordinates.pathPart.push(' -' + 0.2 * this.increment + '  -' + 0.2 * this.increment);
-                coordinates.pathPart.push('l -' + 0.6 * this.increment + ' 0 ');
-                coordinates.pathPart.push('l -' + 0.2 * this.increment + ' 0 ');
+                coordinates.pathParts.push(' -' + 0.2 * this.increment + '  -' + 0.2 * this.increment);
+                coordinates.pathParts.push('l -' + 0.6 * this.increment + ' 0 ');
+                coordinates.pathParts.push('l -' + 0.2 * this.increment + ' 0 ');
                 coordinates.direction = 'left';
             }
             if (lastCoords.direction === 'down') {
-                coordinates.x = this.increment;
-                coordinates.pathPart.push(' ' + 0.2 * this.increment + '  ' + 0.2 * this.increment);
-                coordinates.pathPart.push('l ' + 0.6 * this.increment + ' 0 ');
-                coordinates.pathPart.push('l ' + 0.2 * this.increment + ' 0 ');
+                coordinates.x += this.increment;
+                coordinates.pathParts.push(' ' + 0.2 * this.increment + '  ' + 0.2 * this.increment);
+                coordinates.pathParts.push('l ' + 0.6 * this.increment + ' 0 ');
+                coordinates.pathParts.push('l ' + 0.2 * this.increment + ' 0 ');
                 coordinates.direction = 'right';
             }
             if (lastCoords.direction === 'left') {
                 coordinates.y += this.increment;
-                coordinates.pathPart.push(' -' + 0.2 * this.increment + '  ' + 0.2 * this.increment);
-                coordinates.pathPart.push('l 0 ' + 0.6 * this.increment + ' ');
-                coordinates.pathPart.push('l 0 ' + 0.2 * this.increment + ' ');
+                coordinates.pathParts.push(' -' + 0.2 * this.increment + '  ' + 0.2 * this.increment);
+                coordinates.pathParts.push('l 0 ' + 0.6 * this.increment + ' ');
+                coordinates.pathParts.push('l 0 ' + 0.2 * this.increment + ' ');
                 coordinates.direction = 'down';
             }
             if (lastCoords.direction === 'right') {
                 coordinates.y -= this.increment;
-                coordinates.pathPart.push(' ' + 0.2 * this.increment + '  -' + 0.2 * this.increment);
-                coordinates.pathPart.push('l 0 -' + 0.6 * this.increment + ' ');
-                coordinates.pathPart.push('l 0 -' + 0.2 * this.increment + ' ');
+                coordinates.pathParts.push(' ' + 0.2 * this.increment + '  -' + 0.2 * this.increment);
+                coordinates.pathParts.push('l 0 -' + 0.6 * this.increment + ' ');
+                coordinates.pathParts.push('l 0 -' + 0.2 * this.increment + ' ');
                 coordinates.direction = 'up';
             }
         }
 
         //turn right
         if (newDirection === 'r') {
-            let pop = coordinates.pathPart.pop(); //remove
+            let pop = coordinates.pathParts.pop(); //remove
             if (pop) {
-                coordinates.pathPart.push(pop.replace('l', 'q')); //control point
+                coordinates.pathParts.push(pop.replace('l', 'q')); //control point
             }
             if (lastCoords.direction === 'up') {
-                coordinates.x = this.increment;
-                coordinates.pathPart.push(' ' + 0.2 * this.increment + '  -' + 0.2 * this.increment + ' ');
-                coordinates.pathPart.push('l ' + 0.6 * this.increment + ' 0 ');
-                coordinates.pathPart.push('l ' + 0.2 * this.increment + ' 0 ');
+                coordinates.x += this.increment;
+                coordinates.pathParts.push(' ' + 0.2 * this.increment + '  -' + 0.2 * this.increment + ' ');
+                coordinates.pathParts.push('l ' + 0.6 * this.increment + ' 0 ');
+                coordinates.pathParts.push('l ' + 0.2 * this.increment + ' 0 ');
                 coordinates.direction = 'right';
             }
             if (lastCoords.direction === 'down') {
                 coordinates.x -= this.increment;
-                coordinates.pathPart.push(' -' + 0.2 * this.increment + '  ' + 0.2 * this.increment + ' ');
-                coordinates.pathPart.push('l -' + 0.6 * this.increment + ' 0 ');
-                coordinates.pathPart.push('l -' + 0.2 * this.increment + ' 0 ');
+                coordinates.pathParts.push(' -' + 0.2 * this.increment + '  ' + 0.2 * this.increment + ' ');
+                coordinates.pathParts.push('l -' + 0.6 * this.increment + ' 0 ');
+                coordinates.pathParts.push('l -' + 0.2 * this.increment + ' 0 ');
                 coordinates.direction = 'left';
             }
             if (lastCoords.direction === 'left') {
                 coordinates.y -= this.increment;
-                coordinates.pathPart.push(' -' + 0.2 * this.increment + '  -' + 0.2 * this.increment + ' ');
-                coordinates.pathPart.push('l 0 -' + 0.6 * this.increment + ' ');
-                coordinates.pathPart.push('l 0 -' + 0.2 * this.increment + ' ');
+                coordinates.pathParts.push(' -' + 0.2 * this.increment + '  -' + 0.2 * this.increment + ' ');
+                coordinates.pathParts.push('l 0 -' + 0.6 * this.increment + ' ');
+                coordinates.pathParts.push('l 0 -' + 0.2 * this.increment + ' ');
                 coordinates.direction = 'up';
             }
             if (lastCoords.direction === 'right') {
                 coordinates.y += this.increment;
-                coordinates.pathPart.push(' ' + 0.2 * this.increment + '  ' + 0.2 * this.increment + ' ');
-                coordinates.pathPart.push('l 0 ' + 0.6 * this.increment + ' ');
-                coordinates.pathPart.push('l 0 ' + 0.2 * this.increment + ' ');
+                coordinates.pathParts.push(' ' + 0.2 * this.increment + '  ' + 0.2 * this.increment + ' ');
+                coordinates.pathParts.push('l 0 ' + 0.6 * this.increment + ' ');
+                coordinates.pathParts.push('l 0 ' + 0.2 * this.increment + ' ');
                 coordinates.direction = 'down';
             }
         }
@@ -133,21 +133,20 @@ export class PathGenerator {
      * @param y start y
      * @param commands f/l/r (forward, left, right)
      */
-    generatePath = (x: number, y: number, commands: string[]): string => {
+    generatePath = (x: number, y: number, commands: string[]): coords => {
 
-        let path = `M ${x} ${y} `;
         let target: coords = {
             x: x,
             y: y,
             direction: 'up',
-            pathPart: []
+            pathParts: [`M ${x} ${y} `]
         };
 
         commands.forEach(dir => {
             target = this.nextCoordinates(target, dir);
         })
 
-        console.debug("generated path:", path + target.pathPart.join(' '));
-        return path + target.pathPart.join(' ');
+        console.debug("generated path:", target.pathParts.join(' '));
+        return target;
     }
 }
